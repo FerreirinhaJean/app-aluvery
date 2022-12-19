4a1dc86523bf1e32ad1003cea341d57745f0885d
jean.ferreira <jean.ferreira@compasso.com.br>
2022-12-19 08:56:08 -0300

    Initial commit

---
+++ app/src/main/java/br/com/jean/aluvery/MainActivity.kt
@@ -0,0 +1,113 @@
+package br.com.jean.aluvery
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.BottomCenter
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.RoundRect
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.jean.aluvery.ui.theme.AluveryTheme
+import br.com.jean.aluvery.ui.theme.Purple500
+import br.com.jean.aluvery.ui.theme.Teal200
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AluveryTheme {
+                Surface {
+                    ProductItem()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProductItem() {
+    Surface(
+        Modifier.padding(8.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 4.dp,
+    ) {
+        Column(
+            modifier = Modifier
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)
+        ) {
+            val imageSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(imageSize)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple500,
+                                Teal200
+                            )
+                        )
+                    )
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "Imagem do produto",
+                    Modifier
+                        .size(imageSize)
+                        .offset(y = imageSize / 2)
+                        .clip(shape = CircleShape)
+                        .align(BottomCenter)
+                )
+            }
+            Spacer(modifier = Modifier.height(imageSize / 2))
+            Column(
+                modifier =
+                Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = LoremIpsum(50).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "R$ 14,99",
+                    fontSize = 14.sp,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun ProductItemPreview() {
+    AluveryTheme {
+        Surface {
+            ProductItem()
+        }
+
+    }
+}
